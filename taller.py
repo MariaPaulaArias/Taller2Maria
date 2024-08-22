14cc70de68e70b175f265ec598b395b147b99ac5
MariaPaulaArias <maria.arias@claro.com.co>
2024-08-22 09:25:10 -0500

    Suma con una funcion

--- taller.py
+++ taller.py
@@ -4,4 +4,15 @@ x = 25
 y = 35
 z = x + y
 
+
+
 print("El resultado de sumar x mas y es z y vale: " , z)
+
+#suma del 1 al 10
+
+def suma(num1, num2):
+    num3 = num2 + num1
+    return num3
+
+
+print(suma(10,5))
